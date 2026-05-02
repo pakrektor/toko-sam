e78fed21fe098ca3f370e02cdfa74944cc812e86
Kari Ayam Racer <162875764+pakrektor@users.noreply.github.com>
2026-05-02 19:25:26 +0700

    Update API URL and token environment variables

--- toko-sam/api/get-prices.js
+++ toko-sam/api/get-prices.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
-  const url = process.env.KV_REST_API_URL;
-  const token = process.env.KV_REST_API_TOKEN;
+  const url = process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL;
+  const token =
+    process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN;
 
   if (!url || !token) {
     return res.status(200).json({ success: true, data: {} });
