89030166cefe819851e9208227a431d6189c0b2c
Kari Ayam Racer <162875764+pakrektor@users.noreply.github.com>
2026-05-03 10:32:20 +0700

    Update products.js

--- toko-sam/api/products.js
+++ toko-sam/api/products.js
@@ -1,9 +1,18 @@
+import fetch from "node-fetch";
+import { HttpsProxyAgent } from "https-proxy-agent";
+
 export default async function handler(req, res) {
   const API_URL = "https://warungrebahan.com/api/v1/products";
   const API_KEY = process.env.WARUNG_API_KEY;
 
+  // Proxy Fixie URL
+  const proxyUrl =
+    process.env.FIXIE_URL ||
+    "http://fixie:oAil0FyiUByqHyu@criterium.usefixie.com:80";
+  const proxyAgent = new HttpsProxyAgent(proxyUrl);
+
   try {
-    console.log("⏱️ Sedang menembak API Warung Rebahan...");
+    console.log("⏱️ Sedang menembak API Warung Rebahan lewat Proxy Fixie...");
 
     const response = await fetch(API_URL, {
       method: "POST",
@@ -13,6 +22,7 @@ export default async function handler(req, res) {
       body: JSON.stringify({
         api_key: API_KEY,
       }),
+      agent: proxyAgent,
     });
 
     const data = await response.json();
